5cf8190e0be22c12c4b0d7145e12241a8778b0fd
Wei Huang <Wei.Huang@noaa.gov>
2026-04-14 03:02:11 +0000

    compile succeed on noaacloud

--- bld.sh
+++ bld.sh
@@ -8,6 +8,9 @@ mkdir -p build
 cd build
 rm -rf *
 
+export NetCDF_INCLUDE_DIRS=/usr/include
+export NetCDF_LIBRARIES=/usr/lib/x86_64-linux-gnu/libnetcdf.so
+
 # Set these so CMake's find_package can work automatically
 export CXX=/usr/bin/g++
 export CC=/usr/bin/gcc
@@ -16,8 +19,21 @@ export FC=/usr/bin/gfortran
 
 cmake ../src \
   -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_PREFIX_PATH=/usr/lib/x86_64-linux-gnu/cmake/Qt5 \
-  "-DCMAKE_EXE_LINKER_FLAGS=-Wl,-rpath,/usr/lib/x86_64-linux-gnu -lGL -lGLU -lX11 -lXrender" \
+  "-DCMAKE_EXE_LINKER_FLAGS=-Wl,-rpath,/usr/lib/x86_64-linux-gnu -lOpenGL -lGL -lGLU -lX11 -lXrender -Wl,--copy-dt-needed-entries" \
+  -DCMAKE_CXX_COMPILER_WORKS=1 \
+  -DCMAKE_C_COMPILER_WORKS=1 \
+  -DCMAKE_Fortran_COMPILER_WORKS=1 \
+  -DNetCDF_INCLUDE_DIRS=/usr/include \
+  -DNetCDF_LIBRARIES=/usr/lib/x86_64-linux-gnu/libnetcdf.so \
+  -DCMAKE_INSTALL_RPATH="/usr/lib/x86_64-linux-gnu"
+
+make -j${nproc}
+
+exit 0
+
+cmake ../src \
+  -DCMAKE_BUILD_TYPE=Release \
+  "-DCMAKE_EXE_LINKER_FLAGS=-Wl,-rpath,/usr/lib/x86_64-linux-gnu -lOpenGL -lGL -lGLU -lX11 -lXrender -Wl,--copy-dt-needed-entries" \
   -DCMAKE_CXX_COMPILER_WORKS=1 \
   -DCMAKE_C_COMPILER_WORKS=1 \
   -DCMAKE_Fortran_COMPILER_WORKS=1 \
@@ -27,4 +43,5 @@ cmake ../src \
 
 make -j${nproc}
 
+exit 0
 
