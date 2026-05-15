7513e5e040aaf6ad9be1756b8eac446f83256cd9
Wei Huang <Wei.Huang@noaa.gov>
2026-05-15 22:17:21 +0000

    add yaml

---
+++ src/shared/yamlhandler.h
@@ -0,0 +1,46 @@
+#ifndef _YAMLHandler_H
+#define _YAMLHandler_H
+
+#include <yaml-cpp/yaml.h>
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class YAMLHandler
+{
+    public:
+        YAMLHandler(const char* flnm); 	// Constructor
+       ~YAMLHandler();			// Destructor
+
+        void read_yaml();
+        void write_yaml();
+
+	string get_flnm() { return _flnm; };
+	string get_name() { return _name; };
+	string get_model() { return _model; };
+	string get_version() { return _version; };
+
+	string get_earth_bmp() { return _earth_bmp; };
+	string get_coastline_file() { return _coastline_file; };
+	string get_coastline_resolution() { return _coastline_resolution; };
+
+	vector<string> get_datafiles() { return _datafiles; };
+
+    private:
+        YAML::Node config;
+
+        string _flnm;
+        string _name;
+        string _model;
+        string _version;
+
+	string _earth_bmp;
+	string _coastline_file;
+	string _coastline_resolution;
+
+	vector<string> _datafiles;
+};
+#endif
+
