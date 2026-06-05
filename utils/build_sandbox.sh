50e94f884142b1ee52722e8fad6c2655d20706be
Wei Huang <Wei.Huang@noaa.gov>
2026-06-05 13:10:08 +0000

    move add few utils scripts to utils

---
+++ utils/build_sandbox.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ set -x
+
+ sudo apptainer build --sandbox sandbox_starviewer starviewer.sif
+
---
+++ utils/install-apptainer.sh
@@ -0,0 +1,4 @@
+sudo add-apt-repository -y ppa:apptainer/ppa
+sudo apt update
+sudo apt install -y apptainer
+
